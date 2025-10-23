c8f4342cb08fa34285eb309ce698449fee0e0e05
Wibowo Arindrarto <contact@arindrarto.dev>
2025-10-23 21:46:15 +0200

    Fix provisioning script errors

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,13 +2,14 @@
 
 set -ueo pipefail
 
-SCRIPT_DIR="$(dirname "$0")"
+SCRIPT_DIR="$(readlink -f "$(dirname "$0")")"
+REPO_DIR="$(dirname "${SCRIPT_DIR}")"
 
 MACHINE=${1}
 IP_ADDR=${2}
 SSH_USER=${3:-root}
 
-MACHINE_FILES="${SCRIPT_DIR}/../machines/${MACHINE}/files"
+MACHINE_FILES="${REPO_DIR}/machines/${MACHINE}/files"
 LOCAL_MACHINE_KEY="${MACHINE_FILES}/persist/machine-key.txt"
 PASS_SECRET_PATH="machines/${MACHINE}/luks"
 
@@ -22,9 +23,11 @@ mkdir -p "$(dirname "${LOCAL_MACHINE_KEY}")"
         sed 's/# public key: //'
 )
 
+set -x
+
 nix run github:nix-community/nixos-anywhere -- \
-    --flake ".#${MACHINE}" \
-    --target-machine "${SSH_USER}@${IP_ADDR}" \
-    --generate-hardware-config nixos-generate-config "./machines/${MACHINE}/hardware.nix" \
+    --flake "${REPO_DIR}#${MACHINE}" \
+    --target-host "${SSH_USER}@${IP_ADDR}" \
+    --generate-hardware-config nixos-generate-config "${REPO_DIR}/machines/${MACHINE}/hardware.nix" \
     --disk-encryption-keys /tmp/disk.key <(pass "${PASS_SECRET_PATH}") \
     --extra-files "${MACHINE_FILES}"
