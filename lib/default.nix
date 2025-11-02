d5f5136142248aa73963b8eb721ef4c33be473fc
Wibowo Arindrarto <contact@arindrarto.dev>
2025-11-02 12:54:15 +0100

    Use nixdoc to document lib

--- lib/default.nix
+++ lib/default.nix
@@ -4,46 +4,110 @@
   ...
 }:
 {
-  ## Create a NixOS module option.
-  ##
-  ## ```nix
-  ## lib.mkOpt nixpkgs.lib.types.str "default" "Description"
-  ## ```
-  ##
-  #@ Type -> Any -> String
+  /**
+    Create a NixOS module option.
+
+    # Example
+
+    ```nix
+    mkOpt nixpkgs.lib.types.str "value" "Description"
+    => {
+      _type = "option";
+      default = "value";
+      description = "Description";
+      type = { ... };
+    }
+    ```
+
+    # Type
+
+    ```
+    mkOpt :: Type -> Any -> String -> AttrSet
+    ```
+
+    # Arguments
+
+    **type**
+    : A nixpkgs.lib.types value.
+
+    **default**
+    : The default value for the function.
+
+    **description**
+    : A human-readable description of the option.
+  */
   mkOpt =
     type: default: description:
     lib.mkOption { inherit type default description; };
 
-  ## Create a NixOS module option with a null default.
-  ##
-  ## ```nix
-  ## lib.mkOpt' nixpkgs.lib.types.str "Description"
-  ## ```
-  #@ Type -> String
-  mkOpt' =
-    type: description:
-    lib.mkOption { inherit type description; };
+  /**
+    Create a NixOS module option with a null default.
 
+    # Example
+
+    ```nix
+    mkOpt' nixpkgs.lib.types.str "Description"
+    => {
+      _type = "option";
+      default = null;
+      description = "Description";
+      type = { ... };
+    }
+    ```
+
+    # Type
+
+    ```
+    mkOpt :: Type -> String -> AttrSet
+    ```
+
+    # Arguments
+
+    **type**
+    : A nixpgs.lib.types value.
+
+    **description**
+    : A human-readable description of the option.
+  */
+  mkOpt' = type: description: lib.mkOption { inherit type description; };
+
+  /**
+    Shorthand for enabling a module option.
+
+    # Example
+
+    ```nix
+    enabled
+    => { enable = true; }
+    ```
+
+    # Type
+
+    ```
+    enabled :: AttrSet
+    ```
+  */
   enabled = {
-    ## Shorthand for enabling an option.
-    ##
-    ## ```nix
-    ## services.nginx = enabled;
-    ## ```
-    ##
-    #@ true
     enable = true;
   };
 
+  /**
+    Shorthand for disabling a module option.
+
+    # Example
+
+    ```nix
+    disabled
+    => { enable = false; }
+    ```
+
+    # Type
+
+    ```
+    disabled :: AttrSet
+    ```
+  */
   disabled = {
-    ## Shorthand for disabling an option.
-    ##
-    ## ```nix
-    ## services.nginx = disabled;
-    ## ```
-    ##
-    #@ false
     enable = false;
   };
 }
