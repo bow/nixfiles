eeefb55826adbf6a8c557f0f63255222fb8e674b
Wibowo Arindrarto <contact@arindrarto.dev>
2025-11-16 22:27:19 +0100

    lib(home): Add function to check if desktop is enabled

--- lib/home.nix
+++ lib/home.nix
@@ -1,9 +1,14 @@
 {
   ...
 }:
-{
+rec {
   /**
     Return whether the current config enables Xorg.
   */
   isXorgEnabled = config: config.nixsys.home.desktop.i3.enable;
+
+  /**
+    Return whether the current config enables desktop.
+  */
+  isDesktopEnabled = config: isXorgEnabled config;
 }
