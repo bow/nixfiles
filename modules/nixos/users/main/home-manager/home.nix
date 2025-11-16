dff642a55c46c1beee19abe961077c67af4e22a4
Wibowo Arindrarto <contact@arindrarto.dev>
2025-11-16 14:18:13 +0100

    module(nixsys.home): Add default packages

--- modules/nixos/users/main/home-manager/home.nix
+++ modules/nixos/users/main/home-manager/home.nix
@@ -1,6 +1,7 @@
 {
   user,
   lib,
+  pkgs,
   outputs,
   asStandalone,
   ...
@@ -21,14 +22,140 @@ in
       homeDirectory = user.home-directory;
     };
 
+    home.packages = with pkgs; [
+      age
+      asdf-vm
+      aria2
+      asciidoctor-with-extensions
+      autojump
+      bat
+      btop
+      ccls
+      clangStdenv
+      cloc
+      curl
+      curlie
+      deadnix
+      delve
+      difftastic
+      direnv
+      distrobox
+      dmidecode
+      dnsmasq
+      dnsutils
+      docker
+      docker-buildx
+      docker-compose
+      dos2unix
+      dua
+      duf
+      elinks
+      entr
+      ethtool
+      eza
+      fd
+      file
+      findutils
+      fzf
+      gcc
+      gdb
+      gh
+      ghostty
+      glow
+      gnugrep
+      gnumake
+      gnupatch
+      gnupg
+      gnused
+      go
+      graphviz
+      grpcurl
+      gzip
+      hexyl
+      htop
+      iftop
+      imagemagick
+      inetutils
+      iotop
+      ipcalc
+      iperf3
+      iproute2
+      jq
+      just
+      ldns
+      libvirt
+      lld
+      lldb
+      lshw
+      ltrace
+      lzip
+      minify
+      mpd
+      mtr
+      ncmpcpp
+      nerdctl
+      nixfmt-rfc-style
+      nmap
+      nodejs
+      ntfs3g
+      p7zip
+      packer
+      pass
+      pciutils
+      pdftk
+      poetry
+      pv
+      pyenv
+      python3
+      qemu
+      readline
+      restic
+      ripgrep
+      rustup
+      sequoia-sq
+      shfmt
+      socat
+      starship
+      stow
+      strace
+      stylua
+      sysstat
+      terraform
+      texlab
+      texliveFull
+      tmux
+      tree
+      unrar
+      unzip
+      usbutils
+      uv
+      valgrind
+      vim
+      virt-manager
+      virt-viewer
+      weechat
+      wget
+      which
+      whois
+      wrk
+      xan
+      xz
+      yazi
+      zip
+      zoxide
+      zstd
+
+      nh
+    ];
+
     nixpkgs = mkIf asStandalone {
       overlays = builtins.attrValues outputs.overlays;
       config.allowUnfree = true;
     };
 
+    programs.home-manager.enable = true;
+
     # Reload systemd units on config change.
     systemd.user.startServices = "sd-switch";
-
-    programs.home-manager.enable = true;
   };
 }
