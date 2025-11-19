4e83da4ec3aa417d2b1b21801a743d2c62ab1ef3
Wibowo Arindrarto <contact@arindrarto.dev>
2025-11-19 05:49:38 +0100

    module(home): Add fonts module and move most fonts from nixos module to home-manager

---
+++ modules/home/fonts/default.nix
@@ -0,0 +1,55 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  inherit (lib)
+    mkIf
+    mkEnableOption
+    mkOption
+    types
+    ;
+
+  cfg = config.nixsys.home.fonts;
+in
+{
+  options.nixsys.home.fonts = mkOption {
+    default = { };
+    type = types.submodule {
+      options = {
+        enable = mkEnableOption "nixsys.home.fonts" // {
+          default = true;
+        };
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    fonts = {
+      fontconfig = {
+        enable = true;
+        defaultFonts.monospace = [ "Iosevka SS03" ];
+      };
+    };
+
+    home.packages = with pkgs; [
+      (iosevka-bin.override { variant = "SS03"; })
+      nerd-fonts.droid-sans-mono
+      nerd-fonts.inconsolata
+      nerd-fonts.ubuntu
+      nerd-fonts.ubuntu-sans
+      noto-fonts
+      noto-fonts-emoji
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      siji
+
+      unstable.font-awesome
+
+      local.awesome-terminal-fonts
+      local.titillium-fonts
+    ];
+  };
+}
--- modules/nixos/system/fonts/default.nix
+++ modules/nixos/system/fonts/default.nix
@@ -1,37 +1,13 @@
 {
   config,
-  pkgs,
   lib,
   ...
 }:
 let
   inherit (lib) mkIf;
-  inherit (lib.nixsys.nixos) hasProfileWorkstation;
 in
 {
-  config = mkIf (hasProfileWorkstation config) {
-    fonts = {
-      enableDefaultPackages = true;
-      packages = with pkgs; [
-        (iosevka-bin.override { variant = "SS03"; })
-        nerd-fonts.droid-sans-mono
-        nerd-fonts.inconsolata
-        nerd-fonts.ubuntu
-        nerd-fonts.ubuntu-sans
-        noto-fonts
-        noto-fonts-emoji
-        noto-fonts-cjk-sans
-        noto-fonts-cjk-serif
-        siji
-
-        unstable.font-awesome
-
-        local.awesome-terminal-fonts
-        local.titillium-fonts
-      ];
-      fontconfig.defaultFonts = {
-        monospace = [ "Iosevka SS03" ];
-      };
-    };
+  config = mkIf config.nixsys.enable {
+    fonts.enableDefaultPackages = true;
   };
 }
