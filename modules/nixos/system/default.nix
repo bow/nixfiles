fa2ab23c99a538a1b04474002e9c0505c573ddf9
Wibowo Arindrarto <contact@arindrarto.dev>
2025-11-20 22:09:31 +0100

    module(nixos): Add QMK udev rules

--- modules/nixos/system/default.nix
+++ modules/nixos/system/default.nix
@@ -6,17 +6,21 @@ let
   inherit (lib) mkOption types;
 in
 {
+  imports = [
+    ./workstation.nix
+  ];
+
   options.nixsys.system = mkOption {
     description = "System settings";
     type = types.submodule {
       options = {
+        hostname = mkOption { type = types.str; };
         profile = mkOption {
           type = types.enum [
             "workstation"
             "node"
           ];
         };
-        hostname = mkOption { type = types.str; };
       };
     };
   };
