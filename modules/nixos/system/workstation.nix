83723ad2f668f32d88395bb6499629fe43c1bd93
Wibowo Arindrarto <contact@arindrarto.dev>
2025-11-20 22:11:56 +0100

    module(nixos): Add udev rules for waking up from device

--- modules/nixos/system/workstation.nix
+++ modules/nixos/system/workstation.nix
@@ -15,6 +15,32 @@ in
         enable = true;
         packages = [
           (pkgs.writeTextFile {
+            name = "wake-on-device-udev-rules";
+            destination = "/etc/udev/rules.d/50-wake-on-device.rules";
+            text = ''
+              # Logitech Unifying Receiver - Keyboard
+              ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c52b", ATTR{power/wakeup}="enabled", ATTR{driver/4-1.1.3.1/power/wakeup}="enabled"
+
+              # Keychron Link dongle on dock front
+              ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="d030", ATTR{power/wakeup}="enabled", ATTR{driver/4-1.1.3.4/power/wakeup}="enabled"
+              # Keychron Link dongle on dock back
+              ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="d030", ATTR{power/wakeup}="enabled", ATTR{driver/4-1.1.3.3/power/wakeup}="enabled"
+
+              # Keychron V1 Max cable on laptop
+              ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0913", ATTR{power/wakeup}="enabled", ATTR{driver/6-1/power/wakeup}="enabled"
+              # Keychron V1 Max cable on dock front
+              ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0913", ATTR{power/wakeup}="enabled", ATTR{driver/4-1.1.1.4/power/wakeup}="enabled"
+
+              # Keychron Q1 Max cable on laptop
+              ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0810", ATTR{power/wakeup}="enabled", ATTR{driver/6-1/power/wakeup}="enabled"
+              # Keychron Q1 Max cable on dock front
+              ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0810", ATTR{power/wakeup}="enabled", ATTR{driver/4-1.1.1.4/power/wakeup}="enabled"
+
+              # # Logitech Unifiying Receiver - Mouse
+              # ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c52b", ATTR{power/wakeup}="enabled", ATTR{driver/4-1.1.3.2/power/wakeup}="enabled"
+            '';
+          })
+          (pkgs.writeTextFile {
             name = "qmk-udev-rules";
             destination = "/etc/udev/rules.d/60-qmk.rules";
             text = ''
