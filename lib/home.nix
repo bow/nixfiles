743fdd12a333aea1133c7266f9522ad716115cd3
Wibowo Arindrarto <contact@arindrarto.dev>
2025-11-23 13:50:58 +0100

    lib(home): Add helper functions to check if specific programs are enabled

--- lib/home.nix
+++ lib/home.nix
@@ -11,4 +11,14 @@ rec {
     Return whether the current config enables desktop.
   */
   isDesktopEnabled = config: isXorgEnabled config;
+
+  /**
+    Return whether the current config enables the given program.
+  */
+  isProgramEnabled = config: name: config.nixsys.home.programs.${name}.enable;
+
+  /**
+    Return whether the current config enables neovim.
+  */
+  isNeovimEnabled = config: isProgramEnabled config "neovim";
 }
