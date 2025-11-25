fb7e0566acdd9df2abb4318e20fdbe6a2b3cef1c
Wibowo Arindrarto <contact@arindrarto.dev>
2025-11-25 05:30:38 +0100

    module(system): Remove unused security options

--- modules/nixos/system/security/default.nix
+++ modules/nixos/system/security/default.nix
@@ -4,16 +4,9 @@
   ...
 }:
 let
-  inherit (lib) mkIf mkOption types;
+  inherit (lib) mkIf;
 in
 {
-  options.nixsys.system.security = mkOption {
-    type = types.submodule {
-      options = {
-      };
-    };
-  };
-
   config = mkIf config.nixsys.enable {
     security = {
       sudo = {
